3159f4847b35ab08294b06dc1946f8afeae35671
AnatoliyKant <kant.anatoliy@yandex.ru>
2019-11-18 01:32:17 +0300

    Fixed NioDNS.git version

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ var package = Package(
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
 
         // 📚
-        .package(url: "https://github.com/OpenKitten/NioDNS.git", from: "2.0.0"),
+        .package(url: "https://github.com/OpenKitten/NioDNS.git", from: "1.0.2"),
         
         
     ],
